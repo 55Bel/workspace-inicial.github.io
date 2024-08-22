af74d96f980f628cdbc3373d27b7c8c256c5050e
camilacanzobre24 <camilacanzobre@hotmail.com>
2024-08-22 18:38:41 -0300

    modified:   js/login.js 	modified:   login.html

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+document.getElementById('ingresar').addEventListener('click', function () {
+    let usuario = document.getElementById("usuario").value.trim();
+    let password = document.getElementById("password").value.trim();
+    if(!usuario || !password){
+    showAlertError();
+    }
+});
